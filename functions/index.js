f0aa647721db9b9ca96a168edea5d86693e41103
killerkodycodes <killerbyrd12@gmail.com>
2025-01-12 23:45:49 -0600

    cleanup file

--- functions/index.js
+++ functions/index.js
@@ -7,19 +7,15 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 require('dotenv').config();
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 const nodemailer = require('nodemailer');
 const functions = require('firebase-functions');
 
 const cors = require("cors");
-
 // Initialize the CORS middleware to only allow browser requests from the actual website. 
 const allowedOrigins = [
     "https://dsarmwrestling.com",
     "https://da-shed-armwrestling.web.app",
-    "https://da-shed-armwrestling.firebaseapp.com",
-    "http://127.0.0.1:5000"
+    "https://da-shed-armwrestling.firebaseapp.com"
 ]
 const corsMiddleware = cors({
     origin: (origin, callback) => {
